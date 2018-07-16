0fa2a05e35f60d0dea561aac795c4918df614688
Chao Ma <super11040@gmail.com>
2018-07-16 15:40:11 -0600

    fixed displaying equation of lm in pdf output

--- R/calRaster_v4.R
+++ R/calRaster_v4.R
@@ -80,8 +80,8 @@ calRaster <- function(known, isoscape, mask = NULL, interpMethod = 2, NA.value =
   y = tissue.iso
   xy = data.frame(x,y)
   equation <- function(x) {
-    lm_coef <- list(a = round(coef(x)[1], digits = 2),
-                    b = round(coef(x)[2], digits = 2),
+    lm_coef <- list(a = as.numeric(round(coef(x)[1], digits = 2)),
+                    b = as.numeric(round(coef(x)[2], digits = 2)),
                     r2 = round(summary(x)$r.squared, digits = 2));
     lm_eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(R)^2~"="~r2,lm_coef)
     as.character(as.expression(lm_eq))
