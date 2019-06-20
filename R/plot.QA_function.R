7f415eab524e407dbdddc0f53b802807d0e3dcbc
Gabriel Bowen <gabe.bowen@utah.edu>
2019-06-20 08:58:39 -0600

    Update plot.QA_function.R
    Change plot titles, labels, and some plotting conventions

--- R/plot.QA_function.R
+++ R/plot.QA_function.R
@@ -2,18 +2,18 @@ plot.QA <- function(obj){
   # accuracy by prob
   xx <- seq(0.01, 0.99, 0.01)
   mean <- data.frame(xx, mean = apply(obj$prption_byProb, 2, mean), type = "QA")
-  p <- ggplot(data=mean, aes(x=xx, y=mean,  color = type)) + 
+  p <- ggplot(data=mean, aes(x=xx, y=mean/ncol(obj$val_stations),  color = type)) + 
     geom_line(size=0.5) + 
     xlab("Relative probability interval") + 
-    ylab("Number of validation stations included") + labs(title = "Accuracy by top accumulated probability")
+    ylab("Proportion of validation stations included") + labs(title = "Assignment specificity")
   print(p)
   
   # accuracy by area
   mean <- data.frame(xx, mean = apply(obj$prption_byArea, 2, mean), type = "QA")
-  p <- ggplot(data=mean, aes(x=xx, y=mean,  color = type)) + 
+  p <- ggplot(data=mean, aes(x=xx, y=mean/ncol(obj$val_stations),  color = type)) + 
     geom_line(size=0.5) + 
-    xlab("Relative probability interval") + 
-    ylab("Number of validation stations included") + labs(title= "Accuracy by top percentage of area")
+    xlab("Cumulative probability threshold") + 
+    ylab("Proportion of validation stations included") + labs(title= "Assignment accuracy")
   print(p)
   
   # plot precision
@@ -25,16 +25,18 @@ plot.QA <- function(obj){
   for(i in 1:99){
     mean <- append(mean, mean(precision[0:(length(obj$precision)-1)*99+i]))
   }
-  precision <- data.frame(xx,  mean = mean, type = 'QA')
+  precision <- data.frame(xx,  mean = 1-mean, type = 'QA')
   p <- ggplot(data=precision, aes(x=xx, y=mean,  color = type)) + 
     geom_line(size=0.5) + 
-    xlab("Relative probability interval") + 
-    ylab("Proportion of the total surface") + labs(title = "Precision")
+    xlab("Cumulative probability threshold") + 
+    ylab("Proportion of area excluded") + labs(title = "Spatial precision")
   print(p)
   
   # plot pd
   pd <- data.frame(y=as.numeric(obj$pd_bird_val), type = "QA")
-  p <- ggplot(pd, aes(x=type, y=y, fill=type)) + geom_boxplot() +  guides(fill=FALSE)
-  p + geom_hline(yintercept=obj$random_prob_density)
+  p <- ggplot(pd, aes(x=type, y=y/obj$random_prob_density, fill=type)) + 
+    geom_boxplot() +  guides(fill=FALSE) + xlab("") +
+    ylab("Odds ratio (known origin:random)")
+  print(p)
 }
 
